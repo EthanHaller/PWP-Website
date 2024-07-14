0b9aa845a4b2fbbc68bd114248cb9c52b5c8204a
EthanHaller <ttk4ey@virginia.edu>
2024-07-14 16:51:21 -0400

    setup daisy ui theme and router

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,21 @@ export default {
 		extend: {},
 	},
 	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#224098",
+					secondary: "#ef6c34",
+					accent: "#37cbde",
+					neutral: "#3d4451",
+					"base-100": "#ffffff",
+					info: "#2094f3",
+					success: "#22c55e",
+					warning: "#f59e0b",
+					error: "#ef4444",
+				},
+			},
+		],
+	},
 }
