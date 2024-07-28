3595e22dd2b1d24fd1042591679744fdba86e3f4
EthanHaller <ttk4ey@virginia.edu>
2024-07-28 19:37:27 -0400

    login page styling

--- frontend/src/pages/admin/Login.jsx
+++ frontend/src/pages/admin/Login.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react"
-import { signInWithEmailAndPassword } from "firebase/auth"
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth"
 import { auth } from "../../firebaseConfig"
 import { useNavigate } from "react-router-dom"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/ui/use-toast"
+import { Toaster } from "@/components/ui/toaster"
 
 const Login = () => {
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
-	const [error, setError] = useState("")
+	const [showResetPassword, setShowResetPassword] = useState(false)
 	const navigate = useNavigate()
+	const { toast } = useToast()
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
@@ -15,19 +21,143 @@ const Login = () => {
 			await signInWithEmailAndPassword(auth, email, password)
 			navigate("/admin/dashboard")
 		} catch (error) {
-			setError(error.message)
+            let message = "Error loggin in. Please try again."
+            if (error?.code === "auth/invalid-credential") message = "Incorrect username or password."
+			toast({
+				title: "Error",
+				description: message,
+				variant: "destructive",
+				duration: Infinity,
+			})
+		}
+	}
+
+	const handlePasswordReset = async (e) => {
+        e.preventDefault()
+		if (!email) {
+			toast({
+				title: "Error",
+				description: error.message,
+				variant: "destructive",
+				duration: 2000,
+			})
+			return
+		}
+		try {
+			await sendPasswordResetEmail(auth, email)
+			toast({
+				title: "Success",
+				description: "Password reset email sent. Please check your inbox.",
+				variant: "success",
+				duration: 2000,
+			})
+		} catch (error) {
+			toast({
+				title: "Error",
+				description: "There was an error sending an email to reset your password. Plase try again.",
+				variant: "destructive",
+				duration: Infinity,
+			})
 		}
 	}
 
 	return (
-		<div className="login-container">
-			<h2>Admin Login</h2>
-			{error && <p className="error">{error}</p>}
-			<form onSubmit={handleSubmit}>
-				<input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-				<input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-				<button type="submit">Login</button>
-			</form>
+		<div className="flex">
+			<div className="hidden lg:flex w-1/2 min-h-screen flex-col justify-center items-center">
+				<img src="pwp.svg" alt="Logo" className="h-96" />
+				<h1 className="text-5xl font-bold text-primary mt-8">Welcome, Admin</h1>
+			</div>
+			<div className="w-full lg:w-1/2 min-h-screen container flex flex-col justify-center items-center">
+				<img src="pwp.svg" alt="Logo" className="lg:hidden h-48 mb-8" />
+				<h1 className="lg:hidden text-xl text-center text-primary">Welcome, Admin</h1>
+				{showResetPassword ? (
+					<>
+						<h2 className="text-center text-3xl font-bold tracking-tight text-primary">Reset your password</h2>
+						<form className="mt-8 w-full" onSubmit={handlePasswordReset}>
+							<div className="w-2/3 mx-auto">
+								<div className="mb-4">
+									<Label htmlFor="email" className="sr-only">
+										Email
+									</Label>
+									<Input
+										id="email"
+										name="email"
+										type="text"
+										autoComplete="email"
+										className="relative block w-full appearance-none rounded-t-md border border-input bg-background px-3 py-2 text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
+										placeholder="Email"
+										onChange={(e) => setEmail(e.target.value)}
+									/>
+								</div>
+								<div className="mb-4 text-right">
+									<Button variant="link" type="button" onClick={() => setShowResetPassword(false)} className="text-sm text-primary">
+										Back to sign in
+									</Button>
+								</div>
+							</div>
+							<div className="w-2/3 mx-auto">
+								<Button
+									type="submit"
+									className="group relative flex w-full justify-center rounded-md bg-primary py-2 px-4 text-sm font-medium text-primary-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+								>
+									Send email
+								</Button>
+							</div>
+						</form>
+					</>
+				) : (
+					<>
+						<h2 className="text-center text-3xl font-bold tracking-tight text-primary">Sign in to your account</h2>
+						<form className="mt-8 w-full" onSubmit={handleSubmit}>
+							<div className="w-2/3 mx-auto">
+								<div className="mb-4">
+									<Label htmlFor="email" className="sr-only">
+										Email
+									</Label>
+									<Input
+										id="email"
+										name="email"
+										type="text"
+										autoComplete="email"
+										className="relative block w-full appearance-none rounded-t-md border border-input bg-background px-3 py-2 text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
+										placeholder="Email"
+										onChange={(e) => setEmail(e.target.value)}
+									/>
+								</div>
+								<div className="mb-4">
+									<Label htmlFor="password" className="sr-only">
+										Password
+									</Label>
+									<Input
+										id="password"
+										name="password"
+										type="password"
+										autoComplete="current-password"
+										required
+										className="relative block w-full appearance-none rounded-b-md border border-input bg-background px-3 py-2 text-foreground placeholder-muted-foreground focus:z-10 focus:border-primary focus:outline-none focus:ring-primary sm:text-sm"
+										placeholder="Password"
+										onChange={(e) => setPassword(e.target.value)}
+									/>
+								</div>
+								<div className="mb-4 text-right">
+									<Button variant="link" type="button" onClick={() => setShowResetPassword(true)} className="text-sm text-primary">
+										Forgot your password?
+									</Button>
+								</div>
+							</div>
+							<div className="w-2/3 mx-auto">
+								<Button
+									type="submit"
+									className="group relative flex w-full justify-center rounded-md bg-primary py-2 px-4 text-sm font-medium text-primary-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+								>
+									Sign in
+								</Button>
+							</div>
+						</form>
+					</>
+				)}
+			</div>
+			<Toaster />
 		</div>
 	)
 }
