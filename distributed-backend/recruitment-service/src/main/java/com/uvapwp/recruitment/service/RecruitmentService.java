ed53b965f74434b93822c0d67a80f36bc3b05f84
EthanHaller <ttk4ey@virginia.edu>
2024-11-19 11:56:59 -0500

    refactor: remove unnecessary comments

--- distributed-backend/recruitment-service/src/main/java/com/uvapwp/recruitment/service/RecruitmentService.java
+++ distributed-backend/recruitment-service/src/main/java/com/uvapwp/recruitment/service/RecruitmentService.java
@@ -18,26 +18,21 @@ public class RecruitmentService {
     }
 
     public Map<String, Object> getRecruitmentInfo() throws ExecutionException, InterruptedException {
-        // Reference to the "recruitment" collection and "generalInfo" document
         DocumentReference recruitmentDocRef = firestore.collection("recruitment").document("generalInfo");
         DocumentSnapshot recruitmentDoc = recruitmentDocRef.get().get();
 
-        // Check if the document exists
         if (recruitmentDoc.exists()) {
-            return recruitmentDoc.getData(); // Return the document data
+            return recruitmentDoc.getData();
         } else {
             throw new RuntimeException("No general info found");
         }
     }
 
     public Map<String, Object> updateRecruitmentInfo(Map<String, Object> recruitmentData) throws ExecutionException, InterruptedException {
-        // Reference to the "recruitment" collection and "generalInfo" document
         DocumentReference recruitmentDocRef = firestore.collection("recruitment").document("generalInfo");
 
-        // Update the document with the provided data
         recruitmentDocRef.update(recruitmentData).get();
 
-        // Fetch and return the updated document
         DocumentSnapshot updatedRecruitmentDoc = recruitmentDocRef.get().get();
         return updatedRecruitmentDoc.getData();
     }
