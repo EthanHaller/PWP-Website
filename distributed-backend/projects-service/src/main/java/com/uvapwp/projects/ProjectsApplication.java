030f846bd57d33f5b1ef888d01ccdda0e3fa54c2
EthanHaller <ttk4ey@virginia.edu>
2024-11-21 14:44:55 -0500

    feat: added projects-service

---
+++ distributed-backend/projects-service/src/main/java/com/uvapwp/projects/ProjectsApplication.java
@@ -0,0 +1,27 @@
+package com.uvapwp.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.uvapwp.firebase", "com.uvapwp.projects"})
+public class ProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsApplication.class, args);
+	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
